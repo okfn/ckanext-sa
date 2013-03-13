2e56bace17ad2467bb6c7caf6234af7e3ce1529a
Nigel Babu <nigelbabu@gmail.com>
2013-03-14 00:03:45 +0530

    Inital code dump from datastorer

--- ckanext/sa/commands.py
+++ ckanext/sa/commands.py
@@ -25,6 +25,8 @@ class DataStore(CkanCommand):
     min_args = 0
     max_args = 1
     MAX_PER_PAGE = 50
+    max_content_length = int(config.get('ckanext-archiver.max_content_length',
+        50000000))
 
     DATA_FORMATS = [
         'csv',
@@ -81,7 +83,6 @@ class DataStore(CkanCommand):
             'username': user.get('name'),
             'webstore_url': config.get('ckan.webstore_url')
         }
-        max_content_length = int(config.get('ckanext-archiver.max_content_length', 50000000))
         for package in packages:
             for resource in package.get('resources', []):
                 mimetype = resource['mimetype']
@@ -96,17 +97,113 @@ class DataStore(CkanCommand):
                 logger.info('Datastore resource from resource {0} from '
                             'package {0}'.format(resource['url'],
                                                  package['name']))
-                # TODO: update resource with datastore URL
-                downloaded_resource = download(context, resource,
-                        max_content_length, self.DATA_FORMATS)
-                print downloaded_resource
+                push_to_datastore(context, resource)
                 break
             break
 
 
-    def push_to_datastore(self):
-        # TODO: delete resource from datastore
-        # TODO: upload to datastore
-        pass
+    def push_to_datastore(self, context, resource):
+        result  = download(context, resource,
+                self.max_content_length, self.DATA_FORMATS)
+        content_type = result['headers'].get('content-type', '')\
+                                        .split(';', 1)[0]  # remove parameters
 
+        f = open(result['saved_file'], 'rb')
+        table_sets = AnyTableSet.from_fileobj(f, mimetype=content_type, extension=resource['format'].lower())
 
+        ##only first sheet in xls for time being
+        row_set = table_sets.tables[0]
+        offset, headers = headers_guess(row_set.sample)
+        row_set.register_processor(headers_processor(headers))
+        row_set.register_processor(offset_processor(offset + 1))
+        row_set.register_processor(datetime_procesor())
+
+        logger.info('Header offset: {0}.'.format(offset))
+
+        guessed_types = type_guess(
+            row_set.sample,
+            [
+                messytables.types.StringType,
+                messytables.types.IntegerType,
+                messytables.types.FloatType,
+                messytables.types.DecimalType,
+                messytables.types.DateUtilType
+            ],
+            strict=True
+        )
+        logger.info('Guessed types: {0}'.format(guessed_types))
+        row_set.register_processor(types_processor(guessed_types, strict=True))
+        row_set.register_processor(stringify_processor())
+
+        ckan_url = context['site_url'].rstrip('/')
+
+        datastore_create_request_url = '%s/api/action/datastore_create' % (ckan_url)
+
+        guessed_type_names = [TYPE_MAPPING[type(gt)] for gt in guessed_types]
+
+        def send_request(data):
+            request = {'resource_id': resource['id'],
+                       'fields': [dict(id=name, type=typename) for name, typename in zip(headers, guessed_type_names)],
+                       'records': data}
+            response = requests.post(datastore_create_request_url,
+                             data=json.dumps(request),
+                             headers={'Content-Type': 'application/json',
+                                      'Authorization': context['apikey']},
+                             )
+            check_response_and_retry(response, datastore_create_request_url, logger)
+
+        # Delete any existing data before proceeding. Otherwise 'datastore_create' will
+        # append to the existing datastore. And if the fields have significantly changed,
+        # it may also fail.
+        try:
+            logger.info('Deleting existing datastore (it may not exist): {0}.'.format(resource['id']))
+            response = requests.post('%s/api/action/datastore_delete' % (ckan_url),
+                            data=json.dumps({'resource_id': resource['id']}),
+                            headers={'Content-Type': 'application/json',
+                                    'Authorization': context['apikey']}
+                            )
+            if not response.status_code or response.status_code not in (200, 404):
+                # skips 200 (OK) or 404 (datastore does not exist, no need to delete it)
+                logger.error('Deleting existing datastore failed: {0}'.format(get_response_error(response)))
+                raise DatastorerException("Deleting existing datastore failed.")
+        except requests.exceptions.RequestException as e:
+            logger.error('Deleting existing datastore failed: {0}'.format(str(e)))
+            raise DatastorerException("Deleting existing datastore failed.")
+
+        logger.info('Creating: {0}.'.format(resource['id']))
+
+        # generates chunks of data that can be loaded into ckan
+        # n is the maximum size of a chunk
+        def chunky(iterable, n):
+            it = iter(iterable)
+            while True:
+                chunk = list(
+                    itertools.imap(
+                        dict, itertools.islice(it, n)))
+                if not chunk:
+                    return
+                yield chunk
+
+        count = 0
+        for data in chunky(row_set.dicts(), 100):
+            count += len(data)
+            send_request(data)
+
+        logger.info("There should be {n} entries in {res_id}.".format(n=count, res_id=resource['id']))
+
+        ckan_request_url = ckan_url + '/api/action/resource_update'
+
+        resource.update({
+            'webstore_url': 'active',
+            'webstore_last_updated': datetime.datetime.now().isoformat()
+        })
+
+        response = requests.post(
+            ckan_request_url,
+            data=json.dumps(resource),
+            headers={'Content-Type': 'application/json',
+                     'Authorization': context['apikey']})
+
+        if response.status_code not in (201, 200):
+            raise DatastorerException('Ckan bad response code (%s). Response was %s' %
+                                 (response.status_code, response.content))
